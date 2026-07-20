4a18129398c9b60bee8fa92c9464178e73c50674
Christopher Cherfan <62882003+ChristCherfan@users.noreply.github.com>
2026-07-20 14:46:22 +0200

    Update NTC_general.py

--- NTC_general.py
+++ NTC_general.py
@@ -3,38 +3,30 @@ from scipy import sparse as sparse
 from scipy import special as sp
 from scipy.spatial import distance
 from matplotlib import pyplot as plt
-from voronoi import voronoi_L, voronoi_D, voronoi_Dee  # type: ignore
-from grid_generator import GridSetup  # type: ignore
-from export_data_davidson import export_sparse_matrix_structure, get_psi_correlated_He, get_psi_correlated_H2
+from voronoif import voronoi_L_sym  # type: ignore
+from grid_generatorf import GridSetup  # type: ignore
+from export_data_davidson import *
+from get_correlated_psi import *
 
-def dg1(Z,mu,r):
-    return -(Z-sp.erf(mu*r))
-
-def dg2(mu,r):
-    return 2*(mu/np.sqrt(np.pi))*np.exp(-(mu*r)**2)
-
-def diag_op(f,Z,mu,grid,k):
+def diag_op(f,Z,grid,k):
     d = np.linalg.norm(grid.positions - np.array(grid.alpha[k]), axis=1)
-    return sparse.diags(f(Z,mu,d))
+    return sparse.diags(f(Z,d))
 
-def Vne(Z,mu,grid):
-    transc = lambda Z,m,d: sp.erf(m*d)/d+ dg2(m,d)/2 + (dg1(Z,m,d)**2)/2
+def Vne(Z,grid):
+    transc = lambda Z,d: Z/d
     Vtrans = 0
     for k in range(len(grid.alpha)):
-        Vtrans += diag_op(transc,Z,mu,grid,k)
+        Vtrans += diag_op(transc,Z,grid,k)
     return Vtrans
 
 def r12(grid):
     return distance.cdist(grid.positions,grid.positions,'euclidean').flatten()
 
-def laplacian(Z, mu, grid):
-    nherm = 0
-    for k in range(len(grid.alpha)):
-        nherm += 2 * diag_op(dg1, Z, mu, grid, k) @ voronoi_D(grid.positions, grid.alpha[k])
-    return voronoi_L(grid.positions) + nherm
+def laplacian(Z,grid):
+    return voronoi_L_sym(grid.positions)
         
-def ham1e(Z,mu,grid):
-    return -0.5*laplacian(Z, mu, grid) - Vne(Z, mu, grid)
+def ham1e(Z,grid):
+    return -0.5*laplacian(Z,grid) - Vne(Z, grid)
 
 def erf_over_x(x):
     x = np.asarray(x)
@@ -44,16 +36,12 @@ def erf_over_x(x):
     result[~nonzero] = 2 / np.sqrt(np.pi)
     return result
 
-def Vee(mu,grid):
+def Vee(grid):
     d = r12(grid)
-    return sparse.diags(mu*erf_over_x(mu*d)+ dg2(mu,d)/2 + (dg1(1,mu,d)**2)/2)
+    return sparse.diags(1/d)
 
-def nhermee(mu,grid):
-    d = r12(grid)
-    return -sparse.diags(dg1(1,mu,d))@voronoi_Dee(grid.positions)
-
-def hamiltonian(Z, mu, grid, N):
-    H = ham1e(Z, mu, grid)
+def hamiltonian(Z,grid, N):
+    H = ham1e(Z,grid)
     if N == 1:
         return sparse.csr_matrix(H)
     else:
@@ -65,14 +53,12 @@ def hamiltonian(Z, mu, grid, N):
             for op in ops[1:]:
                 H_local = sparse.kron(H_local, op)
             Ht += H_local
-        return sparse.csr_matrix(Ht) + Vee(mu, grid) + nhermee(mu, grid)
+        return sparse.csr_matrix(Ht) + Vee(grid)
                          
 #_________________________________________________Parameters and grid_________________________________
 Z=1    #CHANGE ATOMIC NUMBER
 
 Ne = 1   #CHANGE NUMBER OF ELECTRONS
-
-mu = 1
 
 Nr = 32
 Nang = 11
@@ -85,7 +71,8 @@ grid = GridSetup(Nr, Nang, alpha)
 grid.plot_grid()
 
 #_____________________________________________________Resolution and plot________________________________________
-H = hamiltonian(Z,mu,grid,Ne)
+
+H = hamiltonian(Z,grid,Ne)
 
 eigenvalues, eigenvectors = np.linalg.eig(H.toarray())
 
@@ -102,7 +89,6 @@ plt.plot(X, eigenvectors[:,0].real, linewidth=0, marker='o', markersize=1)
 plt.show()
 
 #_____________________________________________________Export for Davidson________________________________________
-
 H = hamiltonian(Z, grid, Ne)
 
 np.savetxt("xyz.txt", grid.positions, fmt="%.6f", delimiter=" ", comments='')
